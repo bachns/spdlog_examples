0e1653d7600c221ff0c7064ffc07b05410d55c8d
Bách Nguyễn <bachns.dev@gmail.com>
2021-01-05 09:27:02 +0700

    backtrace & stopwatch

---
+++ Backtrace/Backtrace.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include <spdlog/spdlog.h>
+
+int main()
+{
+	//Nhớ 32 message gần nhất trong bộ đệm, gồm các message debug và trace
+	spdlog::enable_backtrace(32);
+
+	//Thử tạo 100 message loại debug
+	for (int i = 0; i < 100; i++)
+	{
+		spdlog::debug("Message {}", i);
+	}
+
+
+	//Khi có lỗi xảy ra, 
+	//Ta muốn xem lại 32 message gần nhất
+	spdlog::dump_backtrace();
+
+	return 0;
+}
---
+++ Stopwatch/Stopwatch.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+#include <spdlog/spdlog.h>
+#include <spdlog/stopwatch.h>
+
+int main()
+{
+	//Đồng hồ bấm giờ
+	spdlog::stopwatch sw;
+	spdlog::info("Elapsed {}", sw);
+	spdlog::info("Elapsed {:.3}", sw);
+
+	return 0;
+}
